ef445afd8b2cb2aabe1dd58ddd1cd4ac69dbc205
shravan45 <shravanramdurg45@gmail.com>
2021-11-21 21:51:28 -0500

    Roman to Integers

---
+++ src/Driver.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Driver {
+  public static void main(String[] args){
+
+    // Roman To Integer
+    RomanToInteger obj = new RomanToInteger();
+    int answer = obj.romanToInt("DCXCVI");
+    System.out.println(answer);
+  }
+}
---
+++ src/RomanToInteger.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class RomanToInteger {
+  public int romanToInt(String s) {
+    char[] ch = s.toCharArray();
+    List<String> list = new ArrayList<>();
+    for (char c : ch) {
+      list.add(String.valueOf(c));
+    }
+
+    Map<Character, Integer> map = new HashMap<>();
+    map.put('I', 1);
+    map.put('V', 5);
+    map.put('X', 10);
+    map.put('L', 50);
+    map.put('C', 100);
+    map.put('D', 500);
+    map.put('M', 1000);
+
+    int result = 0;
+    for (int i = 0; i < s.length() - 1; i++) {
+      int current = map.get(s.charAt(i));
+      int next = map.get(s.charAt(i + 1));
+      if (current < next) {
+        result += current * -1;
+      } else {
+        result += current;
+      }
+    }
+    result += map.get(s.charAt(s.length() - 1));
+    return result;
+  }
+}
+
